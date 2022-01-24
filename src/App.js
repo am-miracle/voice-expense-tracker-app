b5a9943bdec4276e44803fe501158d7d337c0e7d
am-miracle <judemiraco0@gmail.com>
2022-01-24 21:11:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+
+import Details from './components/Details/Details';
+import Main from './components/Main/Main';
+
+import useStyles from './styles';
+
+function App() {
+  const { grid } = useStyles();
+  
+  return (
+    <div>
+      <Grid className={grid} container spacing={0} alignItems='center' justify='center' style={{ height: '100vh' }}>
+        <Grid item xs={12} sm={4}>
+          <Details title="income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title='expense' />
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Main = () => {
+    const { root, cardContent } = useStyles();
+
+  return (
+      <Card className={root}>
+          <CardHeader title='Expense Tracker' subheader='Powered by speechly' />
+          <CardContent>
+              <Typography align='center' variant='h5'>Total Balance $100</Typography>
+              <Typography variant='subtitle1' style={{ lineHeight: '1.5em', marginTop: '20px' }}>
+                  {/* <InfoCard /> */}
+                  Trying saying: Add income for $100 in category Salary for Monday....
+              </Typography>
+              <Divider />
+              {/* <form /> */}
+          </CardContent>
+          <CardContent className={cardContent}>
+              <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    {/* <List /> */}
+                  </Grid>
+              </Grid>
+          </CardContent>
+      </Card>
+  );
+};
+
+export default Main;
